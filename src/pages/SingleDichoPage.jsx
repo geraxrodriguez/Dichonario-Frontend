4712c41f8e685fa77b869514a8cfa77c89dade90
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-07-29 18:52:56 -0500

    solved problem of line breaks not rendering by adding whitespace property to HTML elements

--- src/pages/SingleDichoPage.jsx
+++ src/pages/SingleDichoPage.jsx
@@ -46,29 +46,29 @@ const SingleDichoPage = () => {
                         <h3 className="text-indigo-800 font-bold mb-1">
                             Literal Translation:
                         </h3>
-                        <p className="text-lg mb-6">
+                        <p className="text-lg mb-6 whitespace-pre-wrap">
                             {dicho.literalMeaning}
                         </p>
 
                         <h3 className="text-indigo-800 font-bold mb-1">
                             Actually Means:
                         </h3>
-                        <p className="text-lg mb-6">
+                        <p className="text-lg mb-6 whitespace-pre-wrap">
                             {dicho.actualMeaning}
                         </p>
 
                         <h3 className="text-indigo-800 font-bold mb-1">
                             History and Origins:
                         </h3>
-                        <p className="text-lg mb-6">
+                        <p className="text-lg mb-6 whitespace-pre-wrap">
                             {dicho.history}
                         </p>
 
                         <h3 className="text-indigo-800 font-bold mb-1">
-                            Example
+                            Example(s)
                         </h3>
 
-                        <p className="text-lg mb-6">
+                        <p className="text-lg mb-6 whitespace-pre-wrap">
                             {dicho.examples}
                         </p>
 
